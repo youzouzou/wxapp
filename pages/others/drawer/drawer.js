47135c76a58b9ec077974c970daaefacd2de38c6
ECHOyougf <1163558472@qq.com>
2017-02-08 11:39:09 +0800

    drawer

--- pages/others/drawer/drawer.js
+++ pages/others/drawer/drawer.js
@@ -1,7 +1,48 @@
 // pages/others/drawer/drawer.js
 Page({
-   showModal: function () {
-    // 显示遮罩层
+  // showModal: function () {
+  //   // 显示遮罩层
+  //   var animation = wx.createAnimation({
+  //     duration: 200,
+  //     timingFunction: "linear",
+  //     delay: 0
+  //   })
+  //   this.animation = animation
+  //   animation.translateY(300).step()
+  //   this.setData({
+  //     animationData: animation.export(),
+  //     showModalStatus: true
+  //   })
+  //   setTimeout(function () {
+  //     animation.translateY(0).step()
+  //     this.setData({
+  //       animationData: animation
+  //     })
+  //   }.bind(this), 200)
+  // },
+  // hideModal: function () {
+  //   // 隐藏遮罩层
+  //   var animation = wx.createAnimation({
+  //     duration: 200,
+  //     timingFunction: "linear",
+  //     delay: 0
+  //   })
+  //   this.animation = animation
+  //   animation.translateY(300).step()
+  //   this.setData({
+  //     animationData: animation.export(),
+  //   })
+  //   setTimeout(function () {
+  //     animation.translateY(0).step()
+  //     this.setData({
+  //       animationData: animation.export(),
+  //       showModalStatus: false
+  //     })
+  //   }.bind(this), 200)
+  // },
+  // showModal和hideModal函数可以合并为一个函数，需要在组件中设置状态值
+  setModalStatus: function (e) {
+    console.log("设置显示状态，1显示0不显示", e.currentTarget.dataset.status);
     var animation = wx.createAnimation({
       duration: 200,
       timingFunction: "linear",
@@ -10,34 +51,27 @@ Page({
     this.animation = animation
     animation.translateY(300).step()
     this.setData({
-      animationData: animation.export(),
-      showModalStatus: true
+      animationData: animation.export()
     })
+    if (e.currentTarget.dataset.status == 1) {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
     setTimeout(function () {
       animation.translateY(0).step()
       this.setData({
-        animationData: animation.export()
+        animationData: animation
       })
-    }.bind(this), 200)
-  },
-  hideModal: function () {
-    // 隐藏遮罩层
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: "linear",
-      delay: 0
-    })
-    this.animation = animation
-    animation.translateY(300).step()
-    this.setData({
-      animationData: animation.export(),
-    })
-    setTimeout(function () {
-      animation.translateY(0).step()
-      this.setData({
-        animationData: animation.export(),
-        showModalStatus: false
-      })
+      if (e.currentTarget.dataset.status == 0) {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
     }.bind(this), 200)
   }
 })
